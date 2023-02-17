9924d43afff18e607be79b8fcafefacab13542e3
Derya Cortuk <53267226+deryacortuk@users.noreply.github.com>
2023-02-17 19:10:27 +0300

    Update urls.py

--- proj/urls.py
+++ proj/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'article'
 
 urlpatterns = [
-path('',views.apiOverview, name="api"),
+# path('',views.apiOverview, name="api"),
  path('article/', views.ArticleListView.as_view(), name='article_list'),
   path('add/',views.ArticleCreate.as_view()),    
  path('article/<id>/', views.ArticleDetailView.as_view(), name='article_detail'),
